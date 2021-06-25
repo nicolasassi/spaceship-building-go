d9f015284811d6d1d14e81a6589d7609689bad9e
Gustavo Saboia <gustavo.saboia@b2wdigital.com>
2021-06-25 11:45:57 -0300

    Cripto methods

--- spaceship/cripto/cripto.go
+++ spaceship/cripto/cripto.go
@@ -4,5 +4,37 @@ const password = "687ds687ds6dsds879sdsdskh"
 
 type Cripto struct {
 	underBruteForceAttack bool
-	tries int
+	tries                 int
+}
+
+func NewCripto() *Cripto {
+	return &Cripto{
+		underBruteForceAttack: false,
+		tries:                 0,
+	}
+}
+
+func (c *Cripto) reset() {
+	c.underBruteForceAttack = false
+	c.tries = 0
+}
+
+func (c *Cripto) GetUnderBruteForceAttack() bool {
+	return c.underBruteForceAttack
+}
+
+func (c *Cripto) CheckPassword(passwdToCheck string) bool {
+	if passwdToCheck != password {
+		c.tries++
+
+		if c.tries > 3 {
+			c.underBruteForceAttack = true
+		}
+
+		return false
+	}
+
+	c.reset()
+
+	return true
 }
